98fe8210c97ab8592e38f6f1d1aebb13cfc7700c
slowjam83 <pipio8310@gmail.com>
2022-07-06 17:38:17 +0900

    1

--- asset/js/video.js
+++ asset/js/video.js
@@ -12,12 +12,12 @@ const audio = videoPlayer.querySelector('.audio')
 //Play and Pause button
 playButton.addEventListener('click', (e) => {
     if (video.paused) {
-        video.play();
         audio.play();
+        video.play();
         e.target.src = './asset/img/pause_white_24dp.svg'
     } else {
-        video.pause();
         audio.pause();
+        video.pause();
         e.target.src = './asset/img/play_arrow_white_24dp.svg'
     }
 })
